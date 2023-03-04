97a44d2d93618411e05007e7003b0985e944f75f
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-04 10:44:09 -0600

    almost done

--- main.js
+++ main.js
@@ -5,24 +5,17 @@ const js = document.getElementById('jsSource');
 let saveData = JSON.parse(localStorage.getItem('savedCode'));
 let iframe = document.getElementById('targetCode');
 if(!localStorage.getItem('savedCode')) {
-    localStorage.setItem('savedCode', JSON.stringify({'html': `<html>
-<head>
-<title>DevPad</title>
-</head>
-<body>
-<h1>Hello!</h1>
-<p>Write HTML, CSS or JavaScript code here and click 'Run Code'.</p>
-</body>
-</html>`, 'css': '/* CSS goes here */', 'js': '// JS code here'}))
+    localStorage.setItem('savedCode', JSON.stringify({'html': `<h1>Hello!</h1>
+<p>Write HTML, CSS or JavaScript code here and click 'Run Code'.</p>`, 'css': '/* CSS goes here */', 'js': '// JS code here'}))
 }
 
 function runCode() {
     var htmlCode = html.value;
     var cssCode = css.value;
-    var jsCode = js.value
+    var jsCode = js.value;
     iframe = (iframe.contentWindow) ? iframe.contentWindow : (iframe.contentDocument.document) ? iframe.contentDocument.document : iframe.contentDocument;
     iframe.document.open();
-    iframe.document.write(content);
+    iframe.document.write(`<html><head><title>DevPad Output</title></head><body>${htmlCode}<script>${jsCode}</body></html>`);
     iframe.document.close();
     return false;
 }
@@ -32,16 +25,17 @@ function clearCode() {
 }
 
 function saveCode() {
-    localStorage.setItem('savedCode', sourceCode.value);
+    localStorage.setItem('savedCode', {'html':html.value,'css':css.value,'js':js.value});
 }
 
 function loadCode() {
-    sourceCode.value = localStorage.getItem('savedCode');
+    let saveData = JSON.parse(localStorage.getItem('savedCode')); 
+    html.value = saveData.html;
+    css.value = saveData.css;
+    js.value = saveData.js;
 }
 
 // Cleaner startup
-html.value = saveData.html;
-css.value = saveData.css;
-js.value = saveData.js;
+loadCode();
 
 runCode();
