c8696431af148e2dcc116f1c3c9e366c2960e183
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-04 10:56:10 -0600

    split up into html,css,js and it works

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 const html = document.getElementById('htmlSource');
 const css = document.getElementById('cssSource');
 const js = document.getElementById('jsSource');
+let timer, timeoutVal = 450;
 
 let saveData = JSON.parse(localStorage.getItem('savedCode'));
-let iframe = document.getElementById('targetCode');
-if(!localStorage.getItem('savedCode')) {
-    localStorage.setItem('savedCode', JSON.stringify({'html': `<h1>Hello!</h1>
-<p>Write HTML, CSS or JavaScript code here and click 'Run Code'.</p>`, 'css': '/* CSS goes here */', 'js': '// JS code here'}))
+if (!localStorage.getItem('savedCode')) {
+    localStorage.setItem('savedCode', JSON.stringify({
+        'html': `<h1>Hello!</h1>
+<p>Write HTML, CSS or JavaScript code here and click 'Run Code'.</p>`, 'css': '/* CSS goes here */', 'js': '// JS code here'
+    }))
 }
 
 function runCode() {
     var htmlCode = html.value;
     var cssCode = css.value;
     var jsCode = js.value;
-    iframe = (iframe.contentWindow) ? iframe.contentWindow : (iframe.contentDocument.document) ? iframe.contentDocument.document : iframe.contentDocument;
+    if(jsCode.includes('alert')) {
+        jsCode = jsCode.replace(/alert\(/g, 'console.log(\'[Alerted Output] \'+');
+    } else if(jsCode.includes('console.log')) {
+        jsCode = jsCode.replace(/console.log\(/g, 'console.log(\'[Output] \'+');
+    }
+    let iframe = document.getElementById('targetCode');
+    iframe = iframe.contentWindow;
     iframe.document.open();
-    iframe.document.write(`<html><head><title>DevPad Output</title></head><body>${htmlCode}<script>${jsCode}</body></html>`);
+    iframe.document.write(`<html><head><title>DevPad Output</title><style>${cssCode}</style></head><body>${htmlCode}<script>${jsCode}</script></body></html>`);
     iframe.document.close();
     return false;
 }
@@ -25,17 +33,43 @@ function clearCode() {
 }
 
 function saveCode() {
-    localStorage.setItem('savedCode', {'html':html.value,'css':css.value,'js':js.value});
+    localStorage.setItem('savedCode',
+        JSON.stringify({
+            'html': html.value,
+            'css': css.value,
+            'js': js.value
+        })
+    );
 }
 
 function loadCode() {
-    let saveData = JSON.parse(localStorage.getItem('savedCode')); 
+    let saveData = JSON.parse(localStorage.getItem('savedCode'));
     html.value = saveData.html;
     css.value = saveData.css;
     js.value = saveData.js;
 }
 
-// Cleaner startup
-loadCode();
+html.addEventListener('keypress', handleKeyPress);
+html.addEventListener('keyup', handleKeyUp);
 
+css.addEventListener('keypress', handleKeyPress);
+css.addEventListener('keyup', handleKeyUp);
+
+js.addEventListener('keypress', handleKeyPress);
+js.addEventListener('keyup', handleKeyUp);
+
+function handleKeyPress(e) {window.clearTimeout(timer);}
+
+function handleKeyUp(e) {
+	window.clearTimeout(timer);
+	timer = window.setTimeout(() => {
+        runCode();
+  }, timeoutVal);
+}
+
+setInterval(function () {
+    saveCode();
+}, 500)
+
+loadCode();
 runCode();
